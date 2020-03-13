2f5a7b4a244745ea44eedc90829c472d6e9545f9
Kenneth Long <kdlong@hep.wisc.edu>
2020-03-13 21:16:24 +0100

    Make scale/pdf/alt scale optional

--- interface/BranchManager.h
+++ interface/BranchManager.h
@@ -10,18 +10,30 @@
 
 
 struct BranchManager {
-    std::vector<TBranch*> branchHolder;
-    std::unordered_map<std::string, TBranch*> specificBranch;
-    TTree* fChain;
+  std::vector<TBranch*> branchHolder;
+  std::unordered_map<std::string, TBranch*> specificBranch;
+  TTree* fChain;
 
-    void SetTree(TTree* fChain_) {
-	fChain = fChain_;
-    } 
+  void SetTree(TTree* fChain_) {
+    fChain = fChain_;
+  } 
 
-    template<typename T>
-    void SetBranch(std::string name, T& holder) {
-	branchHolder.push_back({});
-	fChain->SetBranchAddress(name.c_str(), &holder, &branchHolder.back());
+  template<typename T>
+  void SetBranch(std::string name, T& holder) {
+    branchHolder.push_back({});
+    fChain->SetBranchAddress(name.c_str(), &holder, &branchHolder.back());
+  }
+
+  template<typename T>
+  void SetSpecificBranch(std::string name, T& holder) {
+    if (specificBranch.find(name) != specificBranch.end())
+        specificBranch[name] = {};
+    fChain->SetBranchAddress(name.c_str(), &holder, &specificBranch[name]);
+  }
+
+  void SetEntry(int entry) {
+    for(auto& it: branchHolder) {
+      it->GetEntry(entry);
     }
 
     template<typename T>
@@ -44,10 +56,15 @@ struct BranchManager {
 	specificBranch[name]->GetEntry(entry);
     }
   
-    void CleanUp() {
-	branchHolder.clear();
-	specificBranch.clear();
-    }
+  void CleanUp() {
+    branchHolder.clear();
+    specificBranch.clear();
+  }
+
+  void CleanSpecificBranch(std::string name) {
+    if (specificBranch.find(name) != specificBranch.end())
+        specificBranch.at(name) = nullptr;
+  }
 };
 
 #endif
